cb1d1613f639abdffc90a0f743ea107a49e2b413
DJaxonG <djaxong33@gmail.com>
2025-10-22 13:50:28 -0400

    Update polynomial_horner.py

--- polynomial_horner.py
+++ polynomial_horner.py
@@ -2,11 +2,37 @@ def evaluate_polynomial_horner(degree, x, constant_term, *coefficients):
     # TODO: Implement polynomial evaluation using Horner's method
     # TODO: Print step-by-step evaluation (S0, S1, S2, etc.)
     # TODO: Return final polynomial result
-    pass
+    S = constant_term
+    k = 1
+    n = 0
+    while k <= degree:
+        num = coefficients[n]
+        S = x*S+num
+        k += 1
+    
+    return S
+
+
+
 
 if __name__ == "__main__":
     # TODO: Add main program loop
     # TODO: Get user input for degree, x, constant term, and coefficients
     # TODO: Call evaluate_polynomial_horner function
     # TODO: Ask user if they want to run again
-    pass
+    stuff = "y"
+
+    while(answer=="y"):
+        enterX = int(input("Enter x: "))
+        enterDegree = int(input("Enter degree: "))
+        enterConstant = int(input("Enter constant: "))
+        coefficients = ()
+
+        for i in range(1, enterDegree+1):
+            coefficients += (int(input(f"Coefficient of the x^{i} term" )),)
+        
+        print("")
+        print("P(x) = " + str(evaluate_polynomial_horner(enterDegree, enterX, enterConstant, coefficients)))
+        print("")
+        answer = input("do you want another poly? (y/n): ")
+        print("")
